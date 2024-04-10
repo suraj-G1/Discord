dde6d944cc013b6c4860e96fc19235490044251f
Suraj Gund <surajgund46@gmail.com>
2024-04-10 11:39:08 +0530

    Four Complete

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mulish","sans-serif"],
+      },
+    
+    colors:{
+      deepBlue:"#020421",
+      lightBlue300:"#4b94ed",
+      lighBlue500:"#0b72e7",
+      greenLight:"#61cea6",
+      grayText:"#818s97",
+      lightGray:"#e2e2e2",
+      grayBlue:"#344a6c",
+      deepBlueHead:"#a62f56",
+      gray2:"#525a76",
+      bluebg:"rgb(88,101,242)"
+
+    },
+  },
+},
+  plugins: [],
+}
+
